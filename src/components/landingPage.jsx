be131cdf0d234011792d84ffc0f5f17ed2fa6245
mxsqxxrx <mosquerast3.dev@gmail.com>
2022-12-04 12:49:38 -0500

    challenge

---
+++ src/components/landingPage.jsx
@@ -0,0 +1,61 @@
+import Datetime from 'react-datetime';
+import { useState } from 'react';
+
+import './landingPage.css';
+
+export const LandingPage = () => {
+
+    const [ mathBirthday, setMathBirthday ] = useState(0);
+    const [ age, setAge ] = useState(0);
+    const [ error, setError ] = useState(null);
+
+    // TODO
+    // ** mejorar input datetime
+    // ** test responsive
+
+    const handleChange = (e) => {
+        const dateSelected = new Date(e);
+        const ageInMilliseconds = Date.now() - dateSelected.getTime();
+        if (ageInMilliseconds < 1) {
+            setAge(0);
+            setError('Invalid date selected');
+            return false; 
+        }
+        const ageObject = new Date(ageInMilliseconds);
+        const ageParsed = Math.abs(ageObject.getUTCFullYear() - 1970);
+        const ageMath = Math.pow(10, Math.abs(ageParsed));
+        setError(null)
+        setAge(ageParsed);
+        setMathBirthday(ageMath);
+    }
+
+    return (
+        <div className='page'>
+            <div className='cover'>
+                <h1>Math Birthday</h1>
+                <div>
+                    <label className='text'>Choose your birth date</label>
+                    <Datetime
+                        timeFormat={false}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                {
+                    age > 0 &&
+                        <ul className='information'>
+                            <li>Age: { age }</li>
+                            <li>Math birthday: { mathBirthday }</li>
+                        </ul>
+                }
+                {
+                    error &&
+                        <div className='error'>
+                            { error }
+                        </div>
+                }
+            </div>
+        </div>
+    )
+};
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { LandingPage } from './landingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datetime/css/react-datetime.css';
+import 'bootstrap/dist/js/bootstrap';
+
+import { LandingPage } from './components/landingPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
